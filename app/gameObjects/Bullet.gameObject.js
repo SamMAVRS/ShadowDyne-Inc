9f432c20104db3223f4a7e1a4806da342a41511d
Sam Larsen <sflarsen@live.com>
2024-07-25 01:13:55 -0700

    Player PewPew done

--- app/gameObjects/Bullet.gameObject.js
+++ app/gameObjects/Bullet.gameObject.js
@@ -1,41 +1,98 @@
-const COLORS = {
-    yellow: { color: 0xffcb36, radius: 8, intensity: 0.5 }
+const RESISTENCES = {
+    kenetic: { color: 0xf7f7f7, radius: 8, intensity: 0.5 },
+    thermal: { color: 0xffcb36, radius: 8, intensity: 0.5 },
+    shock: { color: 0x2688ff, radius: 8, intensity: 0.5 },
+    poison: { color: 0x00d170, radius: 8, intensity: 0.5 },
+    bleed: { color: 0xf70505, radius: 8, intensity: 0.5 },
 };
 
 
-// export default class Bullet extends Phaser.GameObjects.Image {
-//     constructor (scene, x, y, img) {
-//         super(scene, x, y, img);
-//         this.img = img;
+
+
+
 
 export default class Bullet extends Phaser.GameObjects.PointLight {
-    constructor (scene, x, y, type = "yellow", player, velocityX, velocityY) {
-        const { color, radius, intensity } = COLORS[type];
+    // constructor (scene, x, y, type = "kenetic", velocity) {
+    // constructor (scene, x, y, type = "kenetic", pointerX, pointerY) {
+    // constructor (scene, x, y, type = "kenetic", player, velocityX, velocityY) {
+    // constructor (scene, x, y, type = "kenetic", velocityX, velocityY) {
+    constructor (scene, x, y, type = "kenetic", velocityX = 0, velocityY = -300) {
+
+        const { color, radius, intensity } = RESISTENCES[type];
         super(scene, x, y, color, radius, intensity);
 
         this.name = "bullet";
-        this.player = player;
-        this.velocityX = velocityX;
-        this.velocityY = velocityY;
+
+
+
+        // this.bullet_resist = {
+        //     kenetic: this.on_kenetic(this),
+        //     thermal: this.on_thermal(this),
+        //     shock: this.on_shock(this),
+        //     poison: this.on_poision(this),
+        //     bleed: this.on_bleed(this),
+        // };
+
+        // this.bullet_resist = {
+        //     kenetic: this.on_kenetic(this.type),
+        //     thermal: this.on_thermal(this.type),
+        //     shock: this.on_shock(this.type),
+        //     poison: this.on_poision(this.type),
+        //     bleed: this.on_bleed(this.type),
+        // };
+
+        // this.bullet_resist = {
+        //     kenetic: this.on_kenetic(RESISTENCES),
+        //     thermal: this.on_thermal(RESISTENCES),
+        //     shock: this.on_shock(RESISTENCES),
+        //     poison: this.on_poision(RESISTENCES),
+        //     bleed: this.on_bleed(RESISTENCES),
+        // };
+
 
 
         // Phaser Stuff
         scene.add.existing(this);
         scene.physics.add.existing(this);
         this.body.setAllowGravity(false);
-        this.body.setVelocityX(this.velocityX);
-        this.body.setVelocityY(this.velocityY);
+
+        this.body.setVelocityX(velocityX);
+        this.body.setVelocityY(velocityY);
+
         this.body.setCircle(6);
         this.body.setOffset(2, 1);
 
-        this.body.setCollideWorldBounds(true);
+        this.body.setCollideWorldBounds(false);
+        // this.body.setCollideWorldBounds(true);          // ------ FOR TESTING ONLY ------ //
         this.body.onWorldBounds = true;
-
 
         // Handlers
         this.init();
-        this.shooting();
+        this.shooting(this.x, this.y, RESISTENCES[type]);
+        setTimeout(() => this.destroy(), 3500);             // DESTROYS BULLETS AFTER SET TIME
     }
+
+
+
+
+
+
+    shooting(x, y, resistence) {
+        // this.bullet_resist[type](x, y, resistence);
+    }
+
+
+
+    on_kenetic(x, y, resistence) {
+        // this.physics.add(x, y, resistence);
+        // this.scene.add(this.scene, x, y, resistence);
+    }
+    on_thermal(x, y, resistence) { }
+    on_shock(x, y, resistence) { }
+    on_poision(x, y, resistence) { }
+    on_bleed(x, y, resistence) { }
+
+
 
 
 
@@ -43,16 +100,21 @@ export default class Bullet extends Phaser.GameObjects.PointLight {
         this.scene.tweens.add({
             targets: this,
             duration: 200,
-            intensity: { from: 0.3, to: 0.7 },
+            intensity: { from: 0.1, to: 0.9 },
             repeat: -1,
+            // repeat: 3,
+            // onComplete: () => {
+            //     this.destroy();
+            // }
         });
     }
 
 
 
-    shooting() {
 
+
+
+    destroy() {
+        super.destroy();
     }
-
-
 }
--- app/scenes/Game.scene.js
+++ app/scenes/Game.scene.js
@@ -6,9 +6,6 @@ import Bullet from "../gameObjects/Bullet.gameObject";
 
 
 
-const PLAYER_SPEED = 2;
-
-
 
 
 
@@ -23,6 +20,9 @@ export default class Game extends Phaser.Scene {
 
 
 
+
+
+
     ///////////////////////////////////////////////////////////////////////////////////////////
     // ------------- MARK: CREATE ------------- // 
     //////////////////////////////////////////////////////////////////////////////////////////
@@ -34,11 +34,11 @@ export default class Game extends Phaser.Scene {
         this.center_height = this.height / 2;
         this.cameras.main.setBackgroundColor('#585858');
 
+
+
         this.handle_player();
         this.handle_enemies();
         this.handle_colliders();
-        this.handle_keyboard_input();
-        this.handle_player_texture();
         this.handle_bullet();
     }
 
@@ -53,23 +53,7 @@ export default class Game extends Phaser.Scene {
 
     handle_player() {
         this.player = new Player(this, this.center_width, this.center_height, "player_n");
-    }
-
-
-
-
-
-
-    handle_player_texture(is_moving) {
-        if (is_moving) {
-            this.player.setTexture("player_a");
-        }
-
-        if (!is_moving) {
-            setTimeout(() => {
-                this.player.setTexture("player_n");
-            }, 200);
-        }
+        this.add.group(this.player);
     }
 
 
@@ -85,11 +69,19 @@ export default class Game extends Phaser.Scene {
 
 
 
+
+
+
     ///////////////////////////////////////////////////////////////////////////////////////////
     // ------------- MARK: H_BULLET ------------- // 
     //////////////////////////////////////////////////////////////////////////////////////////
 
-    handle_bullet() { }
+    handle_bullet() {
+        this.add.group();           // TODO: ----------- DO I NEED THIS???? ---------------------------------
+    }
+
+
+
 
 
 
@@ -101,19 +93,14 @@ export default class Game extends Phaser.Scene {
 
 
 
+
+
+
     ///////////////////////////////////////////////////////////////////////////////////////////
-    // ------------- MARK: CONTROLS ------------- // 
+    // ------------- MARK: GAMEOVER ------------- // 
     //////////////////////////////////////////////////////////////////////////////////////////
 
-    handle_keyboard_input() {
-        this.curser = this.input.keyboard.createCursorKeys();
-        this.W = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-        this.A = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-        this.S = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-        this.D = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-        this.SPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-        this.SHIFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT);
-    }
+
 
 
 
@@ -124,46 +111,19 @@ export default class Game extends Phaser.Scene {
     // ------------- MARK: UPDATE ------------- // 
     //////////////////////////////////////////////////////////////////////////////////////////
 
-    update() {
-        if (this.curser.left.isDown || this.A.isDown) {
-            this.handle_player_texture(true);
-            this.player.flipX = true;
-            this.player.x -= PLAYER_SPEED;
-        }
-        if (this.curser.right.isDown || this.D.isDown) {
-            this.handle_player_texture(true);
-            this.player.flipX = false;
-            this.player.x += PLAYER_SPEED;
-        }
-        if (this.curser.up.isDown || this.W.isDown) {
-            this.handle_player_texture(true);
-            this.player.y -= PLAYER_SPEED;
-        }
-        if (this.curser.down.isDown || this.S.isDown) {
-            this.handle_player_texture(true);
-            this.player.y += PLAYER_SPEED;
-        }
-        if (this.curser.space.isDown || this.SPACE.isDown) {
-            this.handle_player_texture(true);
-        }
-
-
-
-        if (Phaser.Input.Keyboard.JustUp(this.SPACE)) {
-            this.handle_player_texture(false);
-        }
-        if (Phaser.Input.Keyboard.JustUp(this.A)) {
-            this.handle_player_texture(false);
-        }
-        if (Phaser.Input.Keyboard.JustUp(this.D)) {
-            this.handle_player_texture(false);
-        }
-        if (Phaser.Input.Keyboard.JustUp(this.W)) {
-            this.handle_player_texture(false);
-        }
-        if (Phaser.Input.Keyboard.JustUp(this.S)) {
-            this.handle_player_texture(false);
-        }
-
+    update(time, delta) {
+        if (this.player) this.player.update(time, delta);           // Updating PLAYER_OBJ and passing Scenes Start TIME
     }
+
+
+
+
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    // ------------- MARK: DESTROY ------------- // 
+    //////////////////////////////////////////////////////////////////////////////////////////
+
+
+
 }
